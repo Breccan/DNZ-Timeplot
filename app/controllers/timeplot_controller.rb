56c9e28dccb770db2994b2c835c49741b67cc459
Will Marshall <willmarshall@Will.local>
2009-06-20 13:51:09 +1200

    Further setup

--- app/controllers/timeplot_controller.rb
+++ app/controllers/timeplot_controller.rb
@@ -7,4 +7,18 @@ class TimeplotController < ApplicationController
   def timeplot
     
   end
+  
+  def search
+    
+    unless params[:search_text].nil? || params[:search_text].empty?  
+      
+      json = DigitalNz::TimelineSearch.query(params[:search_text]) 
+
+      
+      
+      render :json => json
+    else
+      redirect_to(:controller => 'timeplot', :action => 'timeplot')
+    end
+  end
 end
